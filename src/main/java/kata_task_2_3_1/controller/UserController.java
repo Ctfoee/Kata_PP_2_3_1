fb5866fa9baf2f35401c0d187ca2c4afb990761f
Ctfoee <hs.wks@mail.ru>
2023-06-07 16:15:49 +0300

    123

--- src/main/java/kata_task_2_3_1/controller/UserController.java
+++ src/main/java/kata_task_2_3_1/controller/UserController.java
@@ -25,16 +25,44 @@ public class UserController {
         return "users";
     }
 
-    @RequestMapping("/addNew")
+    // Create
+    @GetMapping("/addNew")
     public String addNewUser(Model model) {
-        User user = new User();
-        model.addAttribute("user", user);
-        return "addNewUser";
+        model.addAttribute("user", new User());
+        return "addNew";
     }
 
     @PostMapping("/addNew")
     public String saveUser(@ModelAttribute("user") User user) {
         userService.addUser(user);
+        return "redirect:/users";
+    }
+
+    //Read
+    @GetMapping("/{id}")
+    public String getUser(@PathVariable("id") int id, Model model) {
+        model.addAttribute("user", userService.getSingleUser(id));
+        return "singleUser";
+    }
+
+    //Update
+    @GetMapping("/{id}/update")
+    public String updateUser(@PathVariable("id") int id, Model model) {
+        User user = userService.getSingleUser(id);
+        model.addAttribute("user", user);
+        return "update";
+    }
+
+    @PatchMapping("/{id}")
+    public String editUser(@ModelAttribute("user") User user) {
+        userService.addUser(user);
+        return "redirect:/users";
+    }
+
+    //Delete
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable("id") int id) {
+        userService.deleteUser(id);
         return "redirect:/users";
     }
 }
